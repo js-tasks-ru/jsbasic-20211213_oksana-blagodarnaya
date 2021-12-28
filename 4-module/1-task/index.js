341e079c79aeb421bfe6ff3ad05acfad29867a58
Oksana <bagodarnaoksana@ukr.net>
2021-12-28 17:31:52 +0200

    Home work module 4 tasks 1-3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  
+  let ul = document.createElement('ul');
+    
+  for (let i = 0; i < friends.length; i++){
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    let name = friends[i].firstName + ' ' + friends[i].lastName;
+    li.innerHTML = name;
+  }
+  
+  return ul;
 }
+
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  
+
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let tdFirst = row.firstElementChild;
+    let tdLast = row.lastElementChild;
+    
+    if (tdLast.dataset.available === undefined) {
+      row.hidden = true;
+    } else {
+      
+        if (tdLast.getAttribute('data-available') === 'true') {
+          row.classList.add('available'); 
+        } else {
+          row.classList.add('unavailable'); 
+        }
+    }
+
+   if (tdLast.previousElementSibling.textContent === "m") {
+      row.classList.add('male');
+    } 
+    if (tdLast.previousElementSibling.textContent === "f") {
+      row.classList.add('female');
+    }
+    
+    if (tdFirst.nextElementSibling.textContent < 18) {
+      row.style = "text-decoration: line-through";
+    }
+    
+  }
+
 }
