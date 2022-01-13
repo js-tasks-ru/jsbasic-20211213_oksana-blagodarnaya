31567f9ff3355beffa34a8a28b07e19c33bd7e4c
Oksana <bagodarnaoksana@ukr.net>
2022-01-13 14:04:54 +0200

    Home work module 6 tasks 1-3, I have mistake in task 3

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,55 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows; 
+    this.render();
   }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.classList.add('UserTable'); 
+
+    this.elem.innerHTML = `
+      <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `;
+    
+    let tbody = this.elem.lastElementChild;
+        
+    this.rows.forEach(el => {
+      let tr = document.createElement('tr');
+      
+      tr.innerHTML = `
+          <td>${el.name}</td>
+          <td>${el.age}</td>
+          <td>${el.salary}</td>
+          <td>${el.city}</td>
+          <td><button class = "remove-button"> X </button> </td>
+        `;
+      tr.classList.add('everyRow');  
+      tbody.append(tr);
+    });
+
+    let rowsInTable = this.elem.querySelectorAll('.everyRow');
+    
+    for (let rowForDlt of rowsInTable){
+      let btnDelete = rowForDlt.querySelector('button');
+      
+      btnDelete.addEventListener('click', (event) => {
+        let target = event.target;
+        console.log(target.closest('.everyRow'));
+        target.closest('.everyRow').remove();
+      });
+    }
+
+  } 
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,61 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.render();
+
+    let btnRight = this.elem.querySelector('.carousel__arrow_right');
+    let btnLeft = this.elem.querySelector('.carousel__arrow_left');
+
+    btnRight.addEventListener('click', this.onClickRight);
+    btnLeft.addEventListener('click', this.onClickLeft);
+    console.log(this.elem);
   }
+
+  render() {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('Carousel'); 
+    
+    this.slides.forEach(el => {
+      
+      this.elem.insertAdjacentHTML("afterBegin", `
+        <!--Кнопки переключения-->
+
+        <div class="carousel__arrow carousel__arrow_right">
+            <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+            <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+
+        <div class="carousel__inner">
+            <!--Верстка 1-ого слайда-->
+
+            <div class="carousel__slide" data-id="penang-shrimp">
+                <img src="/assets/images/carousel/${el.image}" class="carousel__img" alt="slide">
+                <div class="carousel__caption">
+                    <span class="carousel__price">€${el.price.toFixed(2)}</span>
+                    <div class="carousel__title">${el.name}</div>
+                <button type="button" class="carousel__button">
+                    <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                </button>
+                </div>
+            </div>
+        </div>
+      `);
+    });
+  }
+  
+  onClickRight = (event) => {
+    console.log('click');
+    //let customEvent = new CustomEvent('product-add', { bubbles: true, detail: this.slides.id });
+    //this.elem.dispatchEvent(customEvent);
+    
+  }
+
+  onClickLeft = (event) => {
+    console.log('click');
+    
+    
+  }
+
 }
