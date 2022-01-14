e06aeb7f9fe63b8de90042c62599c5f07e694686
Oksana <bagodarnaoksana@ukr.net>
2022-01-14 14:16:24 +0200

    module 6 task 3 (доделанное)

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,43 +1,38 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
     this.render();
-
-    let btnRight = this.elem.querySelector('.carousel__arrow_right');
-    let btnLeft = this.elem.querySelector('.carousel__arrow_left');
-    btnLeft.style.display = 'none';
-        
-    btnRight.addEventListener('click', this.onClickRight);
-    btnLeft.addEventListener('click', this.onClickLeft);
-    //console.log(slides[0]);
-
+    this.onClickAdd();
+    this.btnRight = this.elem.querySelector('.carousel__arrow_right');
+    this.btnLeft = this.elem.querySelector('.carousel__arrow_left');
+    this.btnLeft.style.display = 'none';
+    this.btnRight.addEventListener('click', this.onClickRight);
+    this.btnLeft.addEventListener('click', this.onClickLeft);
+    this.currentSlide = 0;
+    //console.log(this.elem);
   }
-
   render() {
     this.elem = document.createElement('div');
-    this.elem.classList.add('Carousel');
-    this.elem.insertAdjacentHTML("afterBegin", `
+    this.elem.classList.add('carousel');
+    this.elem.insertAdjacentHTML('afterBegin', `
         <!--Кнопки переключения-->
-
         <div class="carousel__arrow carousel__arrow_right">
-            <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+            <img src="/assets/images/icons/angle-icon.svg" alt='icon'>
         </div>
         <div class="carousel__arrow carousel__arrow_left">
             <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
         </div>
+        <div class="carousel__inner">
+        </div>
       `);
-
-    
+    // Слайды должны быть тут
+    let banner = this.elem.querySelector('.carousel__inner');
     this.slides.forEach(el => {
       
-      this.elem.insertAdjacentHTML("afterBegin", `
-        
-        <div class="carousel__inner">
-            <!--Верстка 1-ого слайда-->
-
-            <div class="carousel__slide" data-id="penang-shrimp">
+      banner.insertAdjacentHTML('beforeend', `
+            <div class="carousel__slide" data-id=${el.id}>
                 <img src="/assets/images/carousel/${el.image}" class="carousel__img" alt="slide">
                 <div class="carousel__caption">
                     <span class="carousel__price">€${el.price.toFixed(2)}</span>
@@ -47,84 +42,42 @@ export default class Carousel {
                 </button>
                 </div>
             </div>
-        </div>
       `);
     });
   }
-    
-  onClickRight = (event) => {
-    //console.log('click');
-    let customEvent = new CustomEvent('product-add', { bubbles: true, detail: this.slides.id });
-    this.elem.dispatchEvent(customEvent);
-    let coordinates = 0;
-    let banner = this.elem.querySelector('.carousel__img');
+  onClickRight = () => {
+    let banner = this.elem.querySelector('.carousel__inner');
     let bannerWidth = banner.offsetWidth;
-    console.log(bannerWidth);
-    console.log(event);
-    
-    switch (coordinates) {
-      case coordinates = 0:
-        banner.style.transform = `translateX(${-bannerWidth}px)`;
-        coordinates = -bannerWidth;
-        btnRight.style.display = '';
-        btnLeft.style.display = '';
-        break;
-
-      case coordinates = -bannerWidth:
-        banner.style.transform = `translateX(${-bannerWidth * 2}px)`;
-        coordinates = -bannerWidth * 2;
-        btnRight.style.display = '';
-        btnLeft.style.display = '';
-        break;
-
-      case coordinates = -bannerWidth * 2:
-        banner.style.transform = `translateX(${-bannerWidth * 3}px)`;
-        coordinates = -bannerWidth * 3;
-        btnRight.style.display = 'none';
-        btnLeft.style.display = '';
-        console.log(banner.style.transform, coordinates);
-        break;
-      default:
-        console.log( "Нет таких значений" );
+    this.currentSlide++;
+    let translateWidth = this.currentSlide * bannerWidth;
+    this.btnLeft.style.display = '';
+    if (this.currentSlide === this.slides.length - 1) {
+      this.btnRight.style.display = 'none';
+    } 
+    banner.style.transform = `translateX(${-translateWidth}px)`;
+  }
+  onClickLeft = () => {
+    let banner = this.elem.querySelector('.carousel__inner');
+    let bannerWidth = banner.offsetWidth;
+    this.currentSlide--;
+    let translateWidth =  this.currentSlide * bannerWidth;
+    this.btnRight.style.display = '';
+    if (this.currentSlide === 0) {
+      this.btnLeft.style.display = 'none';
     }
-    
+    banner.style.transform = `translateX(${-translateWidth}px)`;
   }
 
-  onClickLeft = (event) => {
-    console.log('click');
-    let customEvent = new CustomEvent('product-add', { bubbles: true, detail: this.slides.id });
-    this.elem.dispatchEvent(customEvent);
-    let coordinates = 0;
-    let banner = this.elem.querySelector('.carousel__img');
-    let bannerWidth = banner.offsetWidth;
-    
-        
-    switch (coordinates) {
-      case coordinates = -bannerWidth * 3:
-        banner.style.transform = `translateX(${-bannerWidth * 2}px)`;
-        coordinates = -bannerWidth * 2;
-        btnLeft.style.display = '';
-        btnRight.style.display = '';
-        break;
+  onClickAdd () {
+    let btn = this.elem.querySelectorAll('.carousel__button');
+      for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', () => {
+          console.log(this.slides[i].id);
+          let customEvent = new CustomEvent('product-add', { bubbles: true, detail: this.slides[i].id });
+          this.elem.dispatchEvent(customEvent);
 
-      case coordinates = -bannerWidth * 2:
-        banner.style.transform = `translateX(${-bannerWidth}px)`;
-        coordinates = -bannerWidth;
-        btnLeft.style.display = '';
-        btnRight.style.display = '';
-        break;
-
-      case coordinates = -bannerWidth:
-        banner.style.transform = `translateX(${0}px)`;
-        coordinates = 0;
-        btnLeft.style.display = 'none';
-        btnRight.style.display = '';
-        break;
-
-      default:
-        console.log( "Нет таких значений" );
-    }
-    
+        }); 
+      }
   }
-
+  
 }
