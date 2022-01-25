e3d5f18394ef2288ad14cae1a3fee1869f58efca
Oksana <bagodarnaoksana@ukr.net>
2022-01-25 12:10:38 +0200

    All module 8

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,147 +1,54 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
     this.render();
-    
   }
 
   render() {
-    this.elem = createElement('<div class="products-grid"></div>');
-    this.elem.innerHTML = `
-      <div class="products-grid__inner">
-      </div>
-    `;
-    
-    let gridInner = this.elem.querySelector('.products-grid__inner');
-    
-    this.products.forEach(el => {
-      //console.log(el);
-      let ProductCard = document.createElement('div');
-      ProductCard.classList.add('ProductCard');
-      ProductCard.innerHTML = `
-          <div class="card__top">
-              <img src="/assets/images/products/${el.image}" class="card__image" alt="product">
-              <span class="card__price">€${el.price.toFixed(2)}</span>
-          </div>
-          <div class="card__body">
-              <div class="card__title">${el.name}</div>
-              <button type="button" class="card__button">
-                  <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-              </button>
-          </div>
-      `;
-      gridInner.append(ProductCard);
-    });
+    this.elem = createElement(`<div class="products-grid">
+                                <div class="products-grid__inner">
+                                  <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+                                </div>
+                              </div>`);
 
+    this.products.forEach((product) => {
+      let card = new ProductCard(product);
+      this.elem.querySelector(".products-grid__inner").append(card.elem);
+    });
+  }
+
+  renderCards() {
+    this.elem.querySelector(".products-grid__inner").innerHTML = "";
+
+    for (let product of this.products) {
+      // console.log(this.filters.noNuts && product.nuts, 
+      //   this.filters.vegeterianOnly && !product.vegeterian,
+      //   product.spiciness > this.filters.maxSpiciness,
+      //   product.category != this.filters.category && this.filters.category);
+      if (this.filters.noNuts && product.nuts) {
+        continue;
+      }
+      if (this.filters.vegeterianOnly && !product.vegeterian) {
+        continue;
+      }
+      if (product.spiciness > this.filters.maxSpiciness) {
+        continue;
+      }
+      if (product.category != this.filters.category && this.filters.category) {
+        continue;
+      }
+      let card = new ProductCard(product);
+      this.elem.querySelector(".products-grid__inner").append(card.elem);
+    }
   }
 
   updateFilter(filters) {
-    let gridInner = this.elem.querySelector('.products-grid__inner');
-    this.elem.removeChild(gridInner);
-    this.elem.innerHTML = `
-      <div class="products-grid__inner">
-      </div>
-    `;
-    //console.log(this.elem);
-
-    this.products.filter(el => {
-      let gridInner = this.elem.querySelector('.products-grid__inner');
-      //console.log(filters.nuts);
-      //console.log(el.nuts);
-            
-      if (filters.noNuts) { 
-        if (el.nuts !== true) {
-          //console.log(el);
-              let ProductCard = document.createElement('div');
-              ProductCard.classList.add('ProductCard');
-              ProductCard.innerHTML = `
-                  <div class="card__top">
-                      <img src="/assets/images/products/${el.image}" class="card__image" alt="product">
-                      <span class="card__price">€${el.price.toFixed(2)}</span>
-                  </div>
-                  <div class="card__body">
-                      <div class="card__title">${el.name}</div>
-                      <button type="button" class="card__button">
-                          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-                      </button>
-                  </div>
-              `;
-              gridInner.append(ProductCard);
-        } 
-        
-      }
-      
-      if (filters.vegeterianOnly) { 
-        if (el.vegeterian === true) {
-          //console.log(el);
-          let ProductCard = document.createElement('div');
-              ProductCard.classList.add('ProductCard');
-              ProductCard.innerHTML = `
-                  <div class="card__top">
-                      <img src="/assets/images/products/${el.image}" class="card__image" alt="product">
-                      <span class="card__price">€${el.price.toFixed(2)}</span>
-                  </div>
-                  <div class="card__body">
-                      <div class="card__title">${el.name}</div>
-                      <button type="button" class="card__button">
-                          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-                      </button>
-                  </div>
-              `;
-              gridInner.append(ProductCard);
-        }
-      }
-
-      if (filters.maxSpiciness) { 
-        if (el.spiciness <= 2) {
-          //console.log(el);
-          let ProductCard = document.createElement('div');
-              ProductCard.classList.add('ProductCard');
-              ProductCard.innerHTML = `
-                  <div class="card__top">
-                      <img src="/assets/images/products/${el.image}" class="card__image" alt="product">
-                      <span class="card__price">€${el.price.toFixed(2)}</span>
-                  </div>
-                  <div class="card__body">
-                      <div class="card__title">${el.name}</div>
-                      <button type="button" class="card__button">
-                          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-                      </button>
-                  </div>
-              `;
-              gridInner.append(ProductCard);
-        }
-      }
-
-      if (filters.category) { 
-        if (el.category === filters.category) {
-          //console.log(el);
-          let ProductCard = document.createElement('div');
-              ProductCard.classList.add('ProductCard');
-              ProductCard.innerHTML = `
-                  <div class="card__top">
-                      <img src="/assets/images/products/${el.image}" class="card__image" alt="product">
-                      <span class="card__price">€${el.price.toFixed(2)}</span>
-                  </div>
-                  <div class="card__body">
-                      <div class="card__title">${el.name}</div>
-                      <button type="button" class="card__button">
-                          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-                      </button>
-                  </div>
-              `;
-              gridInner.append(ProductCard);
-            
-        }
-      }
-      //console.log(ProductCard);
-
-    });
-   
-    //console.log(this.elem);
-  } 
+    Object.assign(this.filters, filters);
+    this.renderCards();
+  }
 }
--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -3,26 +3,62 @@ export default class Cart {
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
+    
   }
 
   addProduct(product) {
-    // ваш код
+    if (!!product) {
+      let cartItem = this.cartItems.find(
+        (cart) => cart.product.id == product.id
+      );
+      if (cartItem) {
+        cartItem.count++;
+      } else {
+        cartItem = {
+          product: product,
+          count: 1,
+        };
+        this.cartItems.push(cartItem);
+      }
+      // console.log(this.cartItems);
+      this.onProductUpdate(cartItem);
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+        
+    let cartItem = this.cartItems.find((cart) => cart.product.id == productId);
+    cartItem.count += amount;
+    if (cartItem.count == 0) {
+      this.cartItems.splice(this.cartItems.indexOf(cartItem), 1);
+    }
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    if (this.cartItems.length === 0){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getTotalCount() {
-    // ваш код
+    let count = 0;
+    this.cartItems.forEach(item => {
+      count = count + item.count
+    });
+
+    return count;
   }
 
   getTotalPrice() {
-    // ваш код
+    let price = 0;
+    this.cartItems.forEach(item => {
+      price = price + item.product.price * item.count;
+    });
+    //console.log(price);
+    return price;
   }
 
   onProductUpdate(cartItem) {
